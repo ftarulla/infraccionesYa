e278f0d71197d226bdcbd9f3d87ed2699a8922bd
Franciscello <ftarulla@gmail.com>
2018-05-30 21:07:33 -0300

    Nuevas infracciones

--- infracciones.js
+++ infracciones.js
@@ -1,43 +1,42 @@
 
 var exports = module.exports = {};
 
-
 var infracciones = [{
-
         id: 42,
-        fechaHoraRegistro: new Date("August 20, 2016 02:00:01").toUTCString(),
-        fechaHoraActualizacion: new Date("August 20, 2016 02:00:01").toUTCString(),
-        direccionRegistrada: '',
+        patente: "ABC123",
+        fechaHoraRegistro: new Date("April 20, 2018 14:00:01").toUTCString(),
+        fechaHoraActualizacion: new Date("April 21, 2018 10:00:01").toUTCString(),
+        direccionRegistrada: 'Av. Acá Cerquita 345',
         tipoInfraccion: 00,
         montoAPagar: '$1500',
         existeAcarreo: true,
-
     }, {
-
         id: 07,
-        fechaHoraRegistro: new Date("September 18, 2016 18:18:01").toUTCString(),
-        fechaHoraActualizacion: new Date("September 18, 2016 18:18:01").toUTCString(),
-        direccionRegistrada: '',
+        patente: "ABC123",
+        fechaHoraRegistro: new Date("September 18, 2017 18:18:01").toUTCString(),
+        fechaHoraActualizacion: new Date("September 19, 2017 10:18:00").toUTCString(),
+        direccionRegistrada: 'Calle Falsa 123',
         tipoInfraccion: 01,
-        montoAPagar: '$200',
-        existeAcarreo: true,
-
+        montoAPagar: '$2000',
+        existeAcarreo: false,
     }, {
         id: 08,
-        fechaHoraRegistro: new Date("October 19, 2016 18:18:01").toUTCString(),
-        fechaHoraActualizacion: new Date("October 19, 2016 18:18:01").toUTCString(),
-        direccionRegistrada: '',
+        patente: "AAA000",
+        fechaHoraRegistro: new Date("May 17, 2017 11:18:00").toUTCString(),
+        fechaHoraActualizacion: new Date("May 18, 2017 10:30:00").toUTCString(),
+        direccionRegistrada: 'Abbey Road 3',
         tipoInfraccion: 02,
         montoAPagar: '$100000',
-        existeAcarreo: true,
+        existeAcarreo: false,
     }, {
         id: 24,
-        fechaHoraRegistro: new Date("October 19, 2016 18:18:01").toUTCString(),
+        patente: "BBB111",
+        fechaHoraRegistro: new Date("October 19, 2016 18:18:00").toUTCString(),
         fechaHoraActualizacion: new Date("October 19, 2016 18:18:01").toUTCString(),
-        direccionRegistrada: '',
+        direccionRegistrada: 'Av. Siempreviva 742',
         tipoInfraccion: 04,
-        montoAPagar: '$250',
-        existeAcarreo: false,
+        montoAPagar: '$1250',
+        existeAcarreo: true,
     }
 ]
 
